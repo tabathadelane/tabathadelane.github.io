3abafd5a0e0b6acd9117f858a8ffcc7cb89ad3e7
Tabatha Y <tabathadelane@gmail.com>
2020-05-07 16:26:50 -0700

    created router view content templates, started BS Cols

--- portfolio/src/components/Header.vue
+++ portfolio/src/components/Header.vue
@@ -1,7 +1,9 @@
 <template>
-    <div>
-        <b-img  alt="Headshot of Tabatha" src="../assets/img/headshot.jpeg"></b-img>
+    <b-container>
+        <b-row align-h="around">
+            <b-col cols="4"><b-img fluid rounded="circle" alt="Headshot of Tabatha" src="../assets/img/headshot.jpeg"></b-img></b-col>
 
-        <div><h1>I am Tabatha!</h1></div>
-    </div>
+            <b-col cols="8"><h1>I am Tabatha!</h1></b-col>
+        </b-row>
+    </b-container>
 </template>
--- portfolio/src/views/Trversal.vue
+++ portfolio/src/views/Trversal.vue
@@ -1,5 +1,20 @@
 <template>
     <div>
-        <div><h1>Trversal</h1></div>
+        <div> Column left
+            <div><h1>Trversal</h1></div>
+            <div>About</div>
+            <div>
+                <div><h3>Sources:</h3></div>
+                <div>github link</div>
+                <div>url if deployed</div>
+            </div>
+        </div>
+        <div> Column Right
+            <div> Gif/Screenshot </div>
+            <div>
+                <div><h3>Tech Stack:</h3></div>
+                <div> technologies list</div>
+        </div>
+    </div>
     </div>
 </template>
